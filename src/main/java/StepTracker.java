37e097f65b16fd33707c21c34fdf5dcef6c04d5c
jnthnluong <jnthnluong@gmail.com>
2025-04-23 10:53:04 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
- private int myActiveDays, mySteps, myMinSteps,myTotalDays
+ private int myActiveDays, mySteps, myMinSteps,myTotalDays;
  public StepTracker(int mSteps){
   myMinSteps = mSteps;
   myActiveDays = 0;
